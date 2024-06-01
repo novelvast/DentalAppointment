17d11f3ca6c0ea6289a9af095670ca8a27ac691d
Love1KARINA <1648241930@qq.com>
2024-06-01 16:50:56 +0800

    医生取消重新分配逻辑实现，未接入外部数据库，未测试

---
+++ appointment-service/src/main/java/com/microservice/appointmentservice/dto/DoctorDto.java
@@ -0,0 +1,40 @@
+package com.microservice.personalinfoservice.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Zhao
+ * @since 2023-12-14
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class DoctorDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String username;
+
+    private String phone;
+
+    private String email;
+
+    private Integer hospitalId;
+
+    private String name;
+
+    private Integer jobNumber;
+
+    private String department;
+
+    private String photoUrl;
+}
